b097d4067cfd0622e5314f0568e16798bb064856
enikolaeff <123106349+enikolaeff@users.noreply.github.com>
2023-01-19 21:42:23 +0200

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,7 @@
+city = 'Kyiv'
+population = 3
+square = 826.35
+capital = True
+
+print('I live in ' + str(city) + '. The population of the city is above ' + str(population) + ' million people. Its area is ' + str(square) + ' sq. km. ' + str(city) + ' is the capital of Ukraine - it\'s ' + str(capital) + '.')
+print(f'I live in {city}. The population of the city is above {population} million people. Its area is {square} sq. km. {city} is the capital of Ukraine - it\'s {capital}.')
